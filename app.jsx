99a47470f15d4373a83f6803df560d4912ab35f6
Eric Powell <EricPowell85@gmail.com>
2018-06-15 13:52:24 -0400

    search feature added

--- app.jsx
+++ app.jsx
@@ -6,17 +6,44 @@ function PresidentsList(props) {
   return <div id="error">There was a problem retrieving the list of Presidents. Please try again later.</div>;
 }
 
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.searchResult = this.searchResult.bind(this);
+  }
+
+  searchResult(e) {
+    this.props.onSearchResult(e.target.value)
+    
+  }
+
+  render () {
+    return (
+      <form>
+        <input 
+          type="text" 
+          placeholder="Search" 
+          value={this.props.searchText}
+          onChange={this.searchResult}
+        />
+      </form>
+    )
+  }  
+}
 
 class Application extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
       names: [],
+      origNames: [],
+      searchText: "",
       sortBy: "",
       url: "https://presidentdata.azurewebsites.net/api/pres-names"
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.searchResult = this.searchResult.bind(this);
   }
   
   componentDidMount() { 
@@ -25,6 +52,7 @@ class Application extends React.Component {
     .done((data) => {
       let presidents = JSON.parse(data).name;
       this.setState({names: presidents});
+      this.setState({origNames: presidents});
     })
     .fail((err) => {
       console.log("Request FAILED!", err);
@@ -51,12 +79,39 @@ class Application extends React.Component {
       console.log("Request FAILED!", err);
     })
     event.preventDefault();
-  }   
+  }
+  
+  searchResult(target) {
+  
+    function capitolizeFirst(str) {
+      let words = str.split(' ');
+      let caps = words.map((word) => (word[0].toUpperCase() + word.slice(1)))
+      return caps.join(' ');
+    }   
+    
+    let searchText = target.toLowerCase();
+    let searchResult = [];
+    let names = this.state.origNames.slice();
+    
+    names.filter((pres) => {
+      pres = pres.toLowerCase();
+      if (pres.includes(searchText)) {
+        searchResult.push(capitolizeFirst(pres))
+      }
+    })
+    this.setState({searchText: target});
+    this.setState({names: searchResult});   
+  }
   
   render() {  
     return (
-    	<div>
-    	  <form onSubmit={this.handleSubmit}>
+      <div>
+        <Search 
+          searchText={this.state.searchText}
+          onSearchResult={this.searchResult}
+        />
+      
+        <form onSubmit={this.handleSubmit}>
           <label>
             <select value={this.state.sortBy} onChange={this.handleChange}>
               <option value="">None</option>
@@ -67,7 +122,7 @@ class Application extends React.Component {
           <input type="submit" value="Sort" />
         </form>
         <PresidentsList names={this.state.names} />        
-    	</div>      
+      </div>      
     );
   }
   
